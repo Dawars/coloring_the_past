Fix pts transform for global depth scaling

nerfstudio/data/dataparsers/heritage_dataparser.py
@@ -24,6 +24,7 @@ import numpy as np
 import torch
 import pandas as pd
 import yaml
+from matplotlib import pyplot as plt
 from rich.progress import Console, track
 from typing_extensions import Literal
 
@@ -168,6 +169,8 @@ class Heritage(DataParser):
             pts3d_array[pts_id, :3] = torch.from_numpy(pts.xyz)
             error_array[pts_id, 0] = torch.from_numpy(pts.error)
 
+        plt.scatter(*pts3d_array[:, [0, 2]].T, s=1)
+        plt.show()
         # determine mask extension
         mask_ext = ".npy" if list((self.data / "masks").glob("*.npy")) else ".png"
 
@@ -186,9 +189,9 @@ class Heritage(DataParser):
         fg_masks = []
         sparse_pts = []
 
-        flip = torch.eye(3)
-        flip[0, 0] = -1.0
-        flip = flip.double()
+        # flip = torch.eye(3)
+        # flip[0, 0] = -1.0
+        # flip = flip.double()
 
         for filename in file_list:
             if filename not in img_path_to_id.keys():
@@ -221,6 +224,11 @@ class Heritage(DataParser):
             semantic = np.load(semantic_filenames[-1])["arr_0"]  # todo semantic mask???
             is_sky = semantic != 2  # sky id is 2
             fg_masks.append(torch.from_numpy(is_sky).unsqueeze(-1))  # todo fg_mask
+
+            # gt sky mask for psnr eval
+            # mask = cv2.imread(str(Path("/home/dawars/projects/master_thesis/NeuralRecon-W/output") / img.name.replace(".jpg", ".jpg_mask.png")))
+            # mask = mask.astype(np.bool)
+            # fg_masks.append(torch.from_numpy(mask))
 
             # load sparse 3d points for each view
             # visualize pts3d for each image
@@ -297,20 +305,20 @@ class Heritage(DataParser):
         scale = 1.0 / (radius * 1.01)
         origin = np.array(scene_config["origin"]).reshape(1, 3)
         origin = torch.from_numpy(origin)
-        poses[:, :3, 3] -= origin
+        # poses[:, :3, 3] -= origin
         poses[:, :3, 3] *= scale  # enlarge the radius a little bit
 
-        poses, transform = camera_utils.auto_orient_and_center_poses(
-            poses,
-            method=self.config.orientation_method,
-            center_poses=False,
-        )
+        # poses, transform = camera_utils.auto_orient_and_center_poses(
+        #     poses,
+        #     method=self.config.orientation_method,
+        #     center_poses=False,
+        # )
 
-        # scale pts accordingly
+        # scale pts accordingly  # todo commented out for depth scale calc, breaks dto
         for pts in sparse_pts:
-            pts[:, :3] -= origin
+            # pts[:, :3] -= origin
             pts[:, :3] *= scale  # should be the same as pose preprocessing
-            pts[:, :3] = pts[:, :3] @ transform[:3, :3].t() + transform[:3, 3:].t()
+        #     pts[:, :3] = pts[:, :3] @ transform[:3, :3].t() + transform[:3, 3:].t()
 
         # create occupancy grid from sparse points
         points_ori = []
@@ -337,9 +345,9 @@ class Heritage(DataParser):
         points_ori = torch.from_numpy(points_ori).float()
 
         # scale pts accordingly
-        points_ori -= origin
+        # points_ori -= origin
         points_ori[:, :3] *= scale  # should be the same as pose preprocessing
-        points_ori[:, :3] = points_ori[:, :3] @ transform[:3, :3].t() + transform[:3, 3:].t()
+        # points_ori[:, :3] = points_ori[:, :3] @ transform[:3, :3].t() + transform[:3, 3:].t()
 
         print(points_ori.shape)
 
@@ -367,14 +375,14 @@ class Heritage(DataParser):
         voxel_size = 2.0 / grid_size
         quantified_points = torch.floor((filtered_points + 1.0) * grid_size // 2)
 
-        index = quantified_points[:, 0] * grid_size**2 + quantified_points[:, 1] * grid_size + quantified_points[:, 2]
+        index = quantified_points[:, 0] * grid_size ** 2 + quantified_points[:, 1] * grid_size + quantified_points[:, 2]
 
         offset = torch.linspace(-1.0 + voxel_size / 2.0, 1.0 - voxel_size / 2.0, grid_size)
         x, y, z = torch.meshgrid(offset, offset, offset, indexing="ij")
         offset_cube = torch.stack([x, y, z], dim=-1).reshape(-1, 3)
 
         # xyz
-        mask = torch.zeros(grid_size**3, dtype=torch.bool)
+        mask = torch.zeros(grid_size ** 3, dtype=torch.bool)
         mask[index.long()] = True
 
         points_valid = offset_cube[mask]
@@ -441,7 +449,7 @@ class Heritage(DataParser):
 
         if self.config.include_mono_prior:
             metadata["depth_filenames"] = depth_filenames
-            metadata["depth_unit_scale_factor"] = 1e-3  # todo add config and optimize in model option
+            metadata["depth_unit_scale_factor"] = 1
 
             metadata["normal_filenames"] = normal_filenames
 
@@ -451,7 +459,7 @@ class Heritage(DataParser):
             scene_box=scene_box,
             mask_filenames=mask_filenames,
             metadata=metadata,
-            dataparser_transform=transform,  # origin substraction not included
+            # dataparser_transform=transform,  # origin subtraction not included
             dataparser_scale=scale,
         )
 

nerfstudio/data/datasets/generalized_dataset.py
@@ -25,6 +25,7 @@ import numpy as np
 import numpy.typing as npt
 import torch
 from PIL import Image
+from matplotlib import pyplot as plt
 from rich.progress import Console, track
 from torch.utils.data import Dataset
 from torchtyping import TensorType
@@ -110,6 +111,7 @@ class GeneralizedDataset(InputDataset):
             mask_tensor = nonzero_indices
             assert len(mask_tensor) > 0
             data["mask"] = BasicImages([mask_tensor])
+            data["mask_image"] = BasicImages([mask_image])
         metadata = self.get_metadata(data)
         data.update(metadata)
         return data
@@ -119,14 +121,88 @@ class GeneralizedDataset(InputDataset):
 
         image_idx = data["image_idx"]
         height, width, c = data["image"].images[0].shape
-
+        camera = self._dataparser_outputs.cameras
+        camera_to_world = camera.camera_to_worlds[image_idx]
         if "depth_filenames" in self.metadata:
+            plt.imshow(data["image"].images[0]);plt.show()
+            plt.close()
             depth_filepath = self.metadata["depth_filenames"][image_idx]
+            fg_mask = data["fg_mask"].images[0]  # sky mask
+            mask = data["mask_image"].images[0]  # voxel mask
+            sparse_pts = data["sparse_pts"].images[0][:, :3]
+            pts2d = torch.concat((sparse_pts, torch.zeros((sparse_pts.shape[0], 1))), dim=-1)
+
+            # filter pts
+            norm = torch.norm(pts2d, 2, 1)
+            filter = norm < 1.
+            pts2d = pts2d[filter]
+
+            # world space nerf
+            plt.gca().set_aspect('equal', adjustable='box')
+            plt.scatter(*(pts2d[:, [0,1]]).T, s=1, c=pts2d[:,2]);plt.colorbar();plt.show();plt.close()            # x, y sdf coord
+            # plt.scatter(*(pts2d[:, [0,2]]).T, s=1, c=pts2d[:,1]);plt.colorbar();plt.show();plt.close()            # x, z
+            # plt.scatter(*(pts2d[:, [1,2]]).T, s=1, c=pts2d[:,0]);plt.colorbar();plt.show();plt.close()            # y, z
+
+            # world space colmap but up dir changed
+            # transform = torch.eye(4)
+            # transform[:3, :] = self._dataparser_outputs.dataparser_transform
+            # pts2d = pts2d @ transform
+
+            # world space (scaling, shifted origin)
+            # plt.scatter(*(pts2d[:, :2]).T);plt.show();plt.close()
+            pts2d = pts2d / self._dataparser_outputs.dataparser_scale
+            plt.gca().set_aspect('equal', adjustable='box')
+            plt.scatter(*(pts2d[:, :2]).T,  c=pts2d[:,2], s=1);plt.colorbar();plt.show();plt.close()
+
+            # pts2d += torch.tensor([[0.5687, -0.0936, 6.2896, 0]]) # center
+            # plt.scatter(*(pts2d[:, :2]).T);plt.show();plt.close()
+
+
+            # colmap world space
+
+            plt.gca().set_aspect('equal', adjustable='box')
+            plt.scatter(*(pts2d[:, :2]).T,  c=pts2d[:,2], s=1);plt.colorbar();plt.show();plt.close()
+
+            # camera space
+            # camera_to_world[:, 1:3] *= -1
+            camera_to_world[:, 3] /= self._dataparser_outputs.dataparser_scale
+            rot = camera_to_world[:, :3]
+            tr = camera_to_world[:, 3]
+            pts2d = pts2d[:, :3] @ rot.T - rot.T @ tr
+
+            plt.gca().set_aspect('equal', adjustable='box')
+            plt.scatter(*(pts2d[:, [0, 1]]).T, c=pts2d[:,2], s=1);plt.colorbar();plt.title("ortho xy");plt.show();plt.close()
+            # pts2d = pts2d[pts2d[:, 2] < 0]
+
+
+
+            # z division/project
+            pts2d_proj = pts2d / pts2d[:, 2:3]
+
+            # filter pts
+            filter = (pts2d_proj[:, 0] < 2.0) & (pts2d_proj[:, 0] > -2) & (pts2d_proj[:, 1] < 2) & (pts2d_proj[:, 1] > -2)
+
+            plt.gca().set_aspect('equal', adjustable='box')
+            plt.scatter(*(pts2d_proj[filter][:, [0,1]]).T, s=1, c=pts2d[filter][:,2]);plt.title("proj");plt.show();plt.close()
+            # todo filter depth min max
+            # image space
+            proj = torch.tensor([[camera.fx[image_idx], 0, camera.cx[image_idx]],
+                                 [0, camera.fy[image_idx], camera.cy[image_idx]],
+                                 [0, 0, 1],
+                                 ])
+            pts2d_proj = pts2d_proj[:, [0, 1, 2]] @ proj.T
+
+            plt.imshow((mask & fg_mask).cpu().float().numpy()[..., 0])
+            plt.scatter(*(pts2d_proj[:, :2]).T, s=1)
+            plt.show()
+            plt.close()
+
+            del data["mask_image"]
 
             # Scale depth images to meter units and also by scaling applied to cameras
-            scale_factor = self.depth_unit_scale_factor * self._dataparser_outputs.dataparser_scale
+            # scale_factor = self.depth_unit_scale_factor * self._dataparser_outputs.dataparser_scale
             depth_image = get_depth_image_from_path(
-                filepath=depth_filepath, height=height, width=width, scale_factor=scale_factor
+                filepath=depth_filepath, height=height, width=width, sparse_pts=pts2d[:, :2], mask=mask & fg_mask
             )
 
             metadata["depth_image"] = BasicImages([depth_image])
@@ -134,7 +210,6 @@ class GeneralizedDataset(InputDataset):
         if "normal_filenames" in self.metadata:
             normal_filepath = self.metadata["normal_filenames"][image_idx]
 
-            camera_to_world = self._dataparser_outputs.cameras.camera_to_worlds[image_idx]
             normal_image = get_normal_image_from_path(
                 filepath=normal_filepath, height=height, width=width, camera_to_world=camera_to_world
             )