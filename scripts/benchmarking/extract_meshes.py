7aab600888a179544e795ab9a9363a10a4cb1785
Dávid Komorowicz <david.komorowicz@gini.net>
2023-10-03 14:10:55 +0200

    Update extract_meshes.py script

--- scripts/benchmarking/extract_meshes.py
+++ scripts/benchmarking/extract_meshes.py
@@ -12,25 +12,23 @@ from scripts.extract_mesh import ExtractMesh
 save_path = Path("/mnt/hdd/3d_recon/sdfstudio/outputs")
 
 simplify = True
-gate_transform_path = Path(
-    "/home/dawars/projects/sdfstudio/outputs/neus-gate-test/neus-facto-bigmlp/2023-04-04_093809/dataparser_transforms.json")
-nepszinhaz_transform_path = Path(
-    "/home/dawars/projects/sdfstudio/outputs/neus-nepszinhaz/neus-facto-bigmlp/2023-04-11_152645/dataparser_transforms.json")
 
-for ckpt_file in tqdm(save_path.rglob("neus-gate-skip-*/**/step-000100000.ckpt")):
+for training_path in tqdm(save_path.rglob("neusfacto-*/**/nerfstudio_models/")):
+    ckpt_file = sorted(list(training_path.glob("*.ckpt")))[-1]
     config_file = ckpt_file.parent.with_name("config.yml")
     out_path = config_file.with_name("mesh.ply")
 
-    data_transform_path = gate_transform_path
-    # data_transform_path = nepszinhaz_transform_path
-    # data_transform_path = ckpt_file.parent.with_name("dataparser_transforms.json")
+    data_transform_path = ckpt_file.parent.with_name("dataparser_transforms.json")
 
     data_transform = json.loads(data_transform_path.read_text())
 
     config = yaml.load(config_file.read_text(), Loader=yaml.Loader)
 
+    setting = config.pipeline.datamanager.dataparser.setting
+    setting_suffix = '' if setting == '' else f'_{setting}'
     data_path = config.pipeline.datamanager.dataparser.data
-    scene_config = yaml.load((data_path / "config.yaml").read_text(), Loader=yaml.FullLoader)
+
+    scene_config = yaml.load((data_path / f"config{setting_suffix}.yaml").read_text(), Loader=yaml.FullLoader)
     transform = np.array([*data_transform["transform"], [0.0, 0.0, 0.0, 1.0]])
     scale = data_transform["scale"]
 
@@ -41,9 +39,18 @@ for ckpt_file in tqdm(save_path.rglob("neus-gate-skip-*/**/step-000100000.ckpt")
     # bb_min = (bb_min_world @ transform) * scale
     # bb_max = (bb_max_world @ transform) * scale
 
-    extract_mesh = ExtractMesh(config_file, resolution=512, output_path=out_path, simplify_mesh=simplify,
-                               bounding_box_min=(-0.5, -0.5, -0.5, ), bounding_box_max=(0.5, 0.5, 0.5)
-                               )
+    extract_mesh = ExtractMesh(
+        config_file,
+        resolution=512,
+        output_path=out_path,
+        simplify_mesh=simplify,
+        bounding_box_min=(
+            -0.5,
+            -0.5,
+            -0.5,
+        ),
+        bounding_box_max=(0.5, 0.5, 0.5),
+    )
     extract_mesh.main()
 
     del extract_mesh
@@ -57,7 +64,7 @@ for ckpt_file in tqdm(save_path.rglob("neus-gate-skip-*/**/step-000100000.ckpt")
     mesh.apply_translation(np.array(scene_config["origin"]))  # add back origin
 
     mesh.export(config_file.with_name("mesh_sfm.ply"))
-    mesh.apply_transform(np.array(scene_config["sfm2gt"]))
-    mesh.export(config_file.with_name("mesh_gt.ply"))
+    # mesh.apply_transform(np.array(scene_config["sfm2gt"]))
+    # mesh.export(config_file.with_name("mesh_gt.ply"))
 
     print(ckpt_file)
